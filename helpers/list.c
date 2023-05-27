ab45b14c1a694d7702936f163fa9151f0cefdf80
SeawardSalmon6 <rayhanachour@hotmail.com>
2023-05-27 17:36:31 -0300

    feat(list): add list implementations

---
+++ helpers/list.c
@@ -0,0 +1,126 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+
+// ======= List Node
+list_node_t *list_node_create(void *content) {
+  list_node_t *node = (list_node_t *)malloc(sizeof(list_node_t));
+
+  if (!node) {
+    printf("--> Error: Not enough memory to allocate a list node!\n");
+    exit(EXIT_FAILURE);
+  }
+
+  node->content = content;
+  node->next = NULL;
+  node->prev = NULL;
+  return node;
+}
+
+list_node_t *list_remove_head(list_t *list) {
+  list_node_t *node = list->head;
+
+  if (list->head) {
+    if (list->head->next) {
+      list->head->next->prev = NULL;
+    } else {
+      list->tail = NULL;
+    }
+
+    list->head = list->head->next;
+
+    // Free node from list
+    node->prev = NULL;
+    node->next = NULL;
+    list->size--;
+  }
+
+  return node;
+}
+
+list_node_t *list_remove_node(list_t *list, list_node_t *node) {
+  if (node) {
+    if (node->prev) {
+      node->prev->next = node->next;
+    } else if (node == list->head) {
+      list->head = node->next;
+    }
+
+    if (node->next) {
+      node->next->prev = node->prev;
+    } else if (node == list->tail) {
+      list->tail = node->prev;
+    }
+
+    list->size--;
+  }
+}
+
+list_node_t *list_remove_tail(list_t *list) {
+  list_node_t *node = list->tail;
+
+  if (list->tail) {
+    if (list->tail->prev) {
+      list->tail->prev->next = NULL;
+    } else {
+      list->head = NULL;
+    }
+
+    list->tail = list->tail->prev;
+
+    // Free node from list
+    node->prev = NULL;
+    node->next = NULL;
+    list->size--;
+  }
+
+  return node;
+}
+
+// ======= List
+list_t *list_init() {
+  list_t *list = (list_t *)malloc(sizeof(list_t));
+
+  if (!list) {
+    printf("--> Error: Not enough memory to allocate a list!\n");
+    exit(EXIT_FAILURE);
+  }
+
+  list->head = NULL;
+  list->tail = NULL;
+  list->size = 0;
+  return list;
+}
+
+void list_add(list_t *list, void *content) {
+  list_node_t *node = list_node_create(content);
+
+  if (!list->head) {
+    list->head = node;
+  } else {
+    list->tail->next = node;
+    node->prev = list->tail;
+  }
+
+  list->tail = node;
+  list->size++;
+}
+
+void list_is_empty(list_t *list) {
+  return !list->head;
+}
+
+void list_free(list_t *list) {
+  if (list) {
+    list_node_t *cur = list->head;
+    list_node_t *tmp;
+
+    while (cur) {
+      tmp = cur->next;
+      free(cur);
+      cur = tmp;
+    }
+
+    free(list);
+  }
+}
---
+++ helpers/list.h
@@ -0,0 +1,29 @@
+#ifndef OS_SIMULATOR_LIST_H
+#define OS_SIMULATOR_LIST_H
+
+// ======= List Node
+typedef struct ListNode {
+  void *content;
+  struct ListNode *next;
+  struct ListNode *prev;
+} list_node_t;
+
+// ======= List
+typedef struct {
+  list_node_t *head;
+  list_node_t *tail;
+  int size;
+} list_t;
+
+// ====== List Node Prototypes
+list_node_t *list_node_create(void *);
+list_node_t *list_remove_head(list_t *);
+list_node_t *list_remove_node(list_t *, list_node_t *);
+list_node_t *list_remove_tail(list_t *);
+
+// ====== List Prototypes
+list_t *list_init();
+void list_add(list_t *, void *);
+void is_list_empty(list_t *);
+
+#endif // OS_SIMULATOR_LIST_H
